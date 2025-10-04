9654e3eca03f10ee5fd142bb77a488cb9effbb6c
AdarshKR00 <adarshkr182@gmail.com>
2025-10-05 02:59:12 +0530

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,19 @@
+// Central export file for all database models
+import User from './User.js';
+import Course from './Course.js';
+import UserProgress from './UserProgress.js';
+import Package from './Package.js';
+
+export {
+    User,
+    Course,
+    UserProgress,
+    Package
+};
+
+export default {
+    User,
+    Course,
+    UserProgress,
+    Package
+};
